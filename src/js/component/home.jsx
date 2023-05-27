6e9e3afff854ead7a0827db4b2658afb0e3746e0
Lucas Solari <solarilucas92@gmail.com>
2023-05-27 10:56:20 +0000

    inicio project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import "./home.style.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [light, setLight] = useState("");
+
+  return (
+    <div className="grandfather">
+      <div className="father bg-black rounded">
+        <div
+          className={`traffic bg-danger rounded-circle ${
+            light === "red" ? "shine" : ""
+          }`}
+          onClick={() => setLight("red")}
+        ></div>
+        <div
+          className={`traffic bg-danger rounded-circle ${
+            light === "yellow" ? "shine" : ""
+          }`}
+          onClick={() => setLight("yellow")}
+        ></div>
+        <div
+          className={`traffic bg-danger rounded-circle ${
+            light === "green" ? "shine" : ""
+          }`}
+          onClick={() => setLight("green")}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
